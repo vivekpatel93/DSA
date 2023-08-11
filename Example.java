bc506c17bb564dc582985fe8b3d3fa4c66575482
vivekpatel06 <bibekymrao1@gmail.com>
2023-08-11 15:06:26 +0530

    change

--- Example.java
+++ Example.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class Example {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        //new funtions
         // size of data type
         /*
          * System.out.println("size is:"+Byte.SIZE);
